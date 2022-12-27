1cc0ae6146a6ddf78aa7b8852759d760ad607f69
Мария <maryreznik82@gmail.com>
2022-12-27 19:50:18 +0400

    Меняет новую форму на редактированную

--- src/view/destinations.js
+++ src/view/destinations.js
@@ -4,6 +4,8 @@ import { humanizeDate} from '../utils/trip';
 const DATE_FORMAT_TIME = 'HH:mm';
 const DATE_FORMAT_DAY = 'DD MMM';
 
+const DATE_FORMAT = 'DD/MM/YYYY HH:mm';
+
 function createDestinationTemplate(trip, allOffers) {
   const {basePrice, dateFrom, dateTo, destination, type} = trip;
   const {name} = destination;
@@ -11,6 +13,8 @@ function createDestinationTemplate(trip, allOffers) {
   const timeFromHum = humanizeDate(dateFrom, DATE_FORMAT_TIME);
   const timeToHum = humanizeDate(dateTo, DATE_FORMAT_TIME);
   const dateFromHum = humanizeDate(dateFrom, DATE_FORMAT_DAY);
+  const fullDateFromHum = humanizeDate(dateFrom, DATE_FORMAT);
+  const fullDateToHum = humanizeDate(dateTo, DATE_FORMAT);
 
   const allOffersByType = allOffers.find((offer) => offer.type === type);
   const { offers} = allOffersByType;
@@ -33,16 +37,16 @@ function createDestinationTemplate(trip, allOffers) {
   return (
     `<li class="trip-events__item">
     <div class="event">
-      <time class="event__date" datetime="2019-03-18">${dateFromHum}</time>
+      <time class="event__date" datetime="${fullDateFromHum}">${dateFromHum}</time>
       <div class="event__type">
         <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${name}</h3>
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">${timeFromHum}</time>
+          <time class="event__start-time" datetime="${fullDateFromHum}">${timeFromHum}</time>
           &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">${timeToHum}</time>
+          <time class="event__end-time" datetime="${fullDateToHum}">${timeToHum}</time>
         </p>
       </div>
       <p class="event__price">
